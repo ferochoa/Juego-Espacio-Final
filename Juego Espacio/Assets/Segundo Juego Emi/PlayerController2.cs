54957271525c306e0fd019fa330671d337285217
Fer ochoa <fer.ochoa.ar@gmail.com>
2018-04-14 01:27:14 -0300

    s

---
+++ Juego Espacio/Assets/Segundo Juego Emi/PlayerController2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+	void Update()
+	{
+		this.gameObject.transform.Rotate (0,0,-Input.GetAxis("Horizontal") * 50f* Time.deltaTime);
+	}
+}
---
+++ Juego Espacio/Assets/Segundo Juego Emi/Spawnpoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour {
+
+	[SerializeField]
+	private float XLimitRight, XLimitLeft,YLimitUp, YLimitDown,YPosition, speed,dropTime,XDirection, YDirection;
+	private float XInitialPosition;
+	private Vector2 movement;
+	public GameObject asteroid;
+	private float levelTime;
+	private bool timeIsRunning;
+
+	void Start()
+	{		
+		XInitialPosition = Random.Range (XLimitLeft +1, XLimitRight);
+		timeIsRunning = true;
+		StartCoroutine (timer ());
+	}
+	void Update()
+	{
+		motion ();
+	}
+	private void motion()
+	{
+		movement = new Vector2 (XDirection, YDirection);
+		this.gameObject.GetComponent<Rigidbody2D> ().velocity = movement * speed;
+
+		if (this.gameObject.transform.position.x > XLimitRight)
+			XDirection = -1;		
+		if (this.gameObject.transform.position.x < XLimitLeft) 
+			XDirection = 1;	
+		if (this.gameObject.transform.position.y > YLimitUp)
+			YDirection = -1;		
+		if (this.gameObject.transform.position.y < YLimitDown) 
+			YDirection = 1;	
+	}	
+	IEnumerator timer()
+	{
+		while (timeIsRunning == true) {
+			yield return new WaitForSeconds (1);
+			levelTime++;
+			if (levelTime % dropTime == 0)
+				Instantiate (asteroid, this.gameObject.transform.position, this.gameObject.transform.rotation);
+		}
+	}
+}
